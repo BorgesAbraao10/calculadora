cf6944faf35befee8ee183bef36cbd4afbfb7201
BorgesAbraao10 <abraao10clear@gmail.com>
2024-08-12 09:44:06 -0300

    Last update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 import "./App.css";
 
 import logoImg from "./assets/logo-1.png";
 
+// Calculo: alcool / gasolina
+// E se o resultado for menor que 0.7 compensa usar alcool
+
 function App() {
+  const [gasolinaInput, setGasolinaInput] = useState(0);
+  const [alcoolInput, setAlcoolInput] = useState(0);
+
+  function calcular(event: FormEvent) {
+    event.preventDefault();
+
+    const calculo = alcoolInput / gasolinaInput;
+    console.log(calculo);
+
+    if (calculo <= 0.7) {
+      alert("Compensa utilizar alcool");
+    } else {
+      alert("Compensa usar gasolina");
+    }
+  }
   return (
     <div>
       <main className="container">
@@ -14,7 +32,8 @@ function App() {
         />
 
         <h1 className="title">Qual melhor opção?</h1>
-        <form className="form">
+
+        <form className="form" onSubmit={calcular}>
           <label>Álcool (preço por litro): </label>
           <input
             className="input"
@@ -23,6 +42,8 @@ function App() {
             min="1"
             step="0.01"
             required
+            value={alcoolInput}
+            onChange={(e) => setAlcoolInput(Number(e.target.value))}
           />
 
           <label>Gasolina (preço por litro): </label>
@@ -33,10 +54,19 @@ function App() {
             min="1"
             step="0.01"
             required
+            value={gasolinaInput}
+            onChange={(e) => setGasolinaInput(Number(e.target.value))}
           />
 
           <input className="button" type="submit" value="Calcular" />
         </form>
+
+        <section className="result">
+          <h2 className="result-title">Compensa usar alcool</h2>
+
+          <span>Alcool R$ 4.30</span>
+          <span>Gasolina R$ 5.10</span>
+        </section>
       </main>
     </div>
   );
